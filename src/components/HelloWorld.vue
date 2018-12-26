9da25904810104285fabea8e07d8055b500ae6db
sheming1987 <sheming1987@163.com>
2018-12-26 16:05:18 +0800

    vue移动端环境搭建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+   	<div>
+   		<a href="javascript:;">第一次使用</a>
+   		<span>第二次使用</span>
+   	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello{
+	
+	a{
+		color: red;
+		font-size: 30px;
+	}
+	span{
+		color:gray;
+		font-size: 36px;
+	}
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'lib-flexible/flexible.js'
+import fastclick from 'fastclick'  //延迟网页点击的300ms延迟
+Vue.config.productionTip = false
+fastclick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
