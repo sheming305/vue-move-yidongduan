c12d4d2a90c93299c9f4667bfdb6fab7a27d6bee
sheming1987 <sheming1987@163.com>
2019-02-18 10:30:10 +0800

    2.18

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'babel-polyfill'  //支持es6的语法
+import 'babel-polyfill' //支持es6的语法
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './axios'
 import mandMobile from 'mand-mobile'
 import 'mand-mobile/lib/mand-mobile.css'
-import 'lib-flexible/flexible.js'
+import 'lib-flexible/flexible.js' //实现rem自适应
 import 'normalize.css' //为了使浏览器更加一致地呈现所有元素并符合现代标准
-import fastclick from 'fastclick'  //延迟网页点击的300ms延迟
+import fastclick from 'fastclick' //延迟网页点击的300ms延迟
 Vue.config.productionTip = false
 fastclick.attach(document.body)
 Vue.use(mandMobile)
@@ -17,6 +17,8 @@ Vue.use(mandMobile)
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
